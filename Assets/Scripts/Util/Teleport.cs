ee13b5bd10e6809c8a04b485d1cf7a986f09a131
Ruru <isuruyahampath1@gmail.com>
2025-12-11 08:29:29 +0530

    added tp points

---
+++ Assets/Scripts/Util/Teleport.cs
@@ -0,0 +1,64 @@
+using PlasticPipe.PlasticProtocol.Messages;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Teleport : MonoBehaviour
+{
+    [Tooltip("If set, teleport will move this object to the target transform's position.")]
+    public Transform targetTransform;
+
+    [Tooltip("Also match rotation of the target transform when teleporting.")]
+    public bool matchRotation = false;
+    private bool teleportPossible = false;
+
+    public InputActionReference teleportAction;
+
+    private GameObject player;
+
+
+    void OnEnable()
+    {
+        // Prefer assigned action
+        if (teleportAction != null && teleportAction.action != null)
+        {
+            teleportAction.action.performed += TeleportToTarget;
+            teleportAction.action.Enable();
+        }
+    }
+
+    /// <summary>
+    /// Teleport this GameObject to the configured target (transform or position).
+    /// </summary>
+    public void TeleportToTarget(InputAction.CallbackContext context)
+    {
+        if (!teleportPossible) return;
+        if (context.performed == false) return;
+
+        if (targetTransform != null && player!=null)
+        {
+            player.transform.position = targetTransform.position;
+            if (matchRotation)
+                player.transform.rotation = targetTransform.rotation;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("fired");
+
+        if (collision.CompareTag("Player"))
+        {
+            teleportPossible = true;
+            player = collision.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("fired");
+        teleportPossible = false;
+        player = null;
+    }
+
+
+}
